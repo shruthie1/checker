a940f638649d26db9d9366077bd2501ad8199d33
SSK <sheetysai>
2025-06-28 11:02:46 +0530

    hmm okay

--- src/CheckerClass.ts
+++ src/CheckerClass.ts
@@ -165,7 +165,7 @@ export class Checker {
             if ((Date.now() - this.pings[client.clientId]) > (5 * 60 * 1000) && (Date.now() - client.lastPingTime) > (5 * 60 * 1000)) {
                 try {
                     if ((Date.now() - this.pings[client.clientId]) > (8 * 60 * 1000) && (Date.now() - client.lastPingTime) > (7 * 60 * 1000)) {
-                        const url = client.promoteRepl.includes('glitch') ? `${client.promoteRepl}/exit` : client.deployKey;
+                        const url = `${client.promoteRepl}/exit`
                         console.log("trying url :", url)
                         try {
                             await axios.get(client.promoteRepl);
